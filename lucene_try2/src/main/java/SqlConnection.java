280ecea5d8b2792ec935043901d78825ee156c9b
WangJi42 <yaochen.wang@ucdconnect.ie>
2021-03-09 17:14:27 +0800

    A try for lucene
    Lucene is a information retreival tool for java. This is a try for using it to analyzing the queries from users and return the most related data to them.

---
+++ lucene_try2/src/main/java/SqlConnection.java
@@ -0,0 +1,102 @@
+import java.math.BigDecimal;
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SqlConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/estate_viewing";
+    private static final String NAME = "root";
+    private static final String PASSWORD = "ROOT";
+    Connection conn = null;
+
+    public void TheSqlConnection(){
+        //加载驱动
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e){
+            System.out.println("未能成功加载驱动程序,请检查是否导入驱动程序!");
+            e.printStackTrace();
+        }
+
+        try{
+            conn = DriverManager.getConnection(URL, NAME, PASSWORD);
+            System.out.println("获取数据库链接成功");
+
+        }catch (SQLException e){
+            System.out.println("获取数据库连接失败");
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public List<HouseBean> selectAll(){
+        List<HouseBean> houseBeanList = new ArrayList<HouseBean>();
+
+        try{
+            ResultSet rs = null;
+            Statement stmt = null;
+            Connection connection = conn;
+
+            String sql ="select * FROM houses";
+
+            stmt =connection.createStatement();
+            rs=stmt.executeQuery(sql);
+
+            while(rs.next()) {
+                int id = rs.getInt(1);
+                int seller_id = rs.getInt(2);
+                Timestamp launch_date = rs.getTimestamp(3);
+                String address = rs.getString(4);
+                double size = rs.getDouble(5);
+                BigDecimal price = rs.getBigDecimal(6);
+                int has_been_sold = rs.getInt(7);
+                int current_holder_id = rs.getInt(8);
+                int second_hand = rs.getInt(9);
+                String rooms_type = rs.getString(10);
+                int floor = rs.getInt(11);
+                int age = rs.getInt(12);
+                int decoration = rs.getInt(13);
+                int house_type = rs.getInt(14);
+                int property = rs.getInt(15);
+                int face = rs.getInt(16);
+                String remarks = rs.getString(17);
+                int delete_flag = rs.getInt(18);
+
+                if (remarks == null) {
+                    remarks = "";
+//                    System.out.println(remarks + " " + remarks.getClass().toString());
+                }
+
+                HouseBean houseBean = new HouseBean(id,seller_id,launch_date,address,size,price,
+                        has_been_sold,current_holder_id,second_hand,rooms_type,floor,age,decoration,house_type,property,
+                        face,remarks,delete_flag);
+
+                houseBeanList.add(houseBean);
+
+//                System.out.println(houseBean.toString());
+
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        //数据库打开后要关闭
+        if(conn != null){
+            try {
+                conn.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+                conn = null;
+            }
+        }
+            return houseBeanList;
+
+    }
+
+
+}
